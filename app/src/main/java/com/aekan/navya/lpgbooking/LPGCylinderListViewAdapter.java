631553936ec1ac988a11b14c20f0d12477ffba2b
arunramamurthy <arun_r80@yahoo.co.in>
2016-03-12 12:41:45 +0530

    Implemented lpg list with recycler viw

---
+++ app/src/main/java/com/aekan/navya/lpgbooking/LPGCylinderListViewAdapter.java
@@ -0,0 +1,97 @@
+package com.aekan.navya.lpgbooking;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by arunramamurthy on 11/03/16.
+ * Class to create LPGCylinderList.
+ * This class will have the classes for RecyclerView viz. the ViewHolder and the adapter
+ */
+public class LPGCylinderListViewAdapter extends RecyclerView.Adapter<LPGCylinderListViewAdapter.LPGViewHolder> {
+
+    //create a static class for the list of lpg cylinder
+    private static class LPGCylinderListInfo {
+        protected String LPGCylinderName;
+        protected String LPGCylinderCompany;
+        protected String LPGCylinderExpiry;
+
+        //Constructor for LPG Cylinder list
+        public LPGCylinderListInfo (String LPGCylinderNameInit, String LPGCylinderCompanyInit,String LPGCylinderExpiryInit) {
+            LPGCylinderName = LPGCylinderNameInit;
+            LPGCylinderCompany = LPGCylinderCompanyInit;
+            LPGCylinderExpiry = LPGCylinderExpiryInit;
+
+        }
+    }
+
+    //Create view holder class
+    public static class LPGViewHolder extends RecyclerView.ViewHolder{
+        //Create the view holder test
+        protected TextView mARLPGName;
+        protected TextView marLPGCompanyName;
+        protected TextView marLPGExpiry;
+
+        public LPGViewHolder(View v){
+            super(v);
+            //assign the view holder elements in constructor
+            mARLPGName = (TextView) v.findViewById(R.id.lpg_cylinder_name);
+            marLPGCompanyName = (TextView) v.findViewById(R.id.lpg_cylinder_company);
+            marLPGExpiry = (TextView) v.findViewById(R.id.lpg_expiry);
+        }
+
+    }
+    //have a fixed list of cylinders for now
+    private final int LPG_CYLINDER_LIST_LENGTH = 4;
+
+    //create a private enumeration for lpg cylinder
+    private ArrayList<LPGCylinderListInfo> LPGCylinderList;
+
+    //Create constructor for the adapter to initialise with
+    public LPGCylinderListViewAdapter(){
+        LPGCylinderList = new ArrayList<LPGCylinderListInfo>();
+        //Initialise the list of values for the array
+        for (int i=0;i<LPG_CYLINDER_LIST_LENGTH;i++)
+        {
+            //Initialise the Arraylist
+            LPGCylinderList.add(new LPGCylinderListInfo("Cylinder no " + i,
+                    "Cylinder Company is Cylinder " + i,
+                    "Cylinder will expiry in " + i + " days"
+            ));
+        }
+    }
+
+    //Implement Create view
+    @Override
+    public LPGViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        //Create the view for this view
+        View LPGListItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.lpg_card_view,parent, false);
+
+        return (new LPGViewHolder(LPGListItem));
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(LPGViewHolder LVH, int i){
+        //get the contact list for the LPG view holder
+        LPGCylinderListInfo CurrentRow = LPGCylinderList.get(i);
+
+        //Assign the text value for each of the item
+        LVH.mARLPGName.setText(CurrentRow.LPGCylinderName);
+        LVH.marLPGCompanyName.setText(CurrentRow.LPGCylinderCompany);
+        LVH.marLPGExpiry.setText(CurrentRow.LPGCylinderExpiry);
+    }
+
+    @Override
+    public int getItemCount(){
+        return LPG_CYLINDER_LIST_LENGTH;
+    }
+}
--- app/src/main/java/com/aekan/navya/lpgbooking/MainActivity.java
+++ app/src/main/java/com/aekan/navya/lpgbooking/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -28,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
 
             //Test commit - dummy comment
         });
+
+        ///////////////////////////////////////
+        //Create recycler view and initialize it
+        ///////////////////////////////////////
+
+        RecyclerView recyclerView= (RecyclerView) findViewById(R.id.lpg_recycler_view);
+        //set recycler view to have same size
+        recyclerView.setHasFixedSize(true);
+        //set layout manager
+        LinearLayoutManager LPGLinearLayoutMgr = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(LPGLinearLayoutMgr);
+        //Set view holder adapter
+        recyclerView.setAdapter(new LPGCylinderListViewAdapter());
+
+
     }
 
     @Override
