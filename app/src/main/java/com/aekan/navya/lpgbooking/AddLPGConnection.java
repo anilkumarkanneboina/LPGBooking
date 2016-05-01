683aca3f8bf6a56249a667b3e23a4bee3d5a02ab
arunramamurthy <arun_r80@yahoo.co.in>
2016-05-01 21:20:14 +0530

    Completion - Addition of new fields in Add connection page, UI fixes,

--- app/src/main/java/com/aekan/navya/lpgbooking/AddLPGConnection.java
+++ app/src/main/java/com/aekan/navya/lpgbooking/AddLPGConnection.java
@@ -118,14 +118,14 @@ public class AddLPGConnection extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+      /*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
